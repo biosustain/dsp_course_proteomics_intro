5bfd22cd4f7fbbcba5e1cf308ede08b47cbb457d
Henry Webel <heweb@dtu.dk>
2025-05-13 19:19:54 +0200

    :art: backlink to repo in sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ html_theme = "sphinx_book_theme"
 # html_logo = "_static/logo-wide.svg"
 # html_favicon = "_static/logo-square.svg"
 html_theme_options = {
-    "github_url": "https://github.com/biosustain/",
+    "github_url": "https://github.com/biosustain/dsp_nextflow_training",
     "repository_url": "https://github.com/biosustain/dsp_nextflow_training",
     # "repository_branch": "main",
     # "home_page_in_toc": True,
