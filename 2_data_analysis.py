21a487bc7f98f688c7ca24f6259f20793096c5c0
Henry Webel <heweb@dtu.dk>
2026-06-02 13:00:40 +0000

    :art: set environment for notebook (default in workspace)

--- 2_data_analysis.py
+++ 2_data_analysis.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.19.1
+#       jupytext_version: 1.19.3
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: base
 #     language: python
 #     name: python3
 # ---
